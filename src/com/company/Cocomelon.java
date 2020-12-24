f7599a15681a449117c3bc1f4893c0ad3c960ac2
ekaraoglu <64097926+ekaraoglu@users.noreply.github.com>
2020-12-23 22:49:05 -0700

    added more lines

--- src/com/company/Cocomelon.java
+++ src/com/company/Cocomelon.java
@@ -5,5 +5,7 @@ public class Cocomelon {
     public static void main(String[] args) {
 
         System.out.println("Happy Holidays");
+        System.out.println("See you next year");
+        System.out.println("Don't worry be happy");
     }
 }
